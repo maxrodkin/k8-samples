4d24310a1098bfd66d8773633920723989253e06
maxrodkin <maxrodkin@gmail.com>
2021-05-21 15:18:43 +0300

    init

--- clone-git-repo.sh
+++ clone-git-repo.sh
@@ -2,16 +2,19 @@
 mkdir -p ~/.ssh
 
 sudo yum -y install git
-aws s3 cp s3://terraform-aws-enconnect/ssh/rodkin-nitka.pem ~/.ssh/
-chmod 400 ~/.ssh/rodkin-nitka.pem
+aws s3 cp s3://ssh-rodkin-nitka/id_rsa ~/.ssh/
+chmod 400 ~/.ssh/id_rsa
 
 touch ~/.ssh/known_hosts
 ssh-keygen -F github.com || ssh-keyscan github.com >>~/.ssh/known_hosts
 
-eval "$(ssh-agent -s)" && ssh-add ~/.ssh/rodkin-nitka.pem
+eval "$(ssh-agent -s)" && ssh-add ~/.ssh/id_rda
+
+touch ~/.ssh/known_hosts
+ssh-keygen -F github.com || ssh-keyscan github.com >>~/.ssh/known_hosts
+
 cd $1
-#git clone --single-branch --branch with-DB-creating  git@github.com:enVerid/ops.git
-git clone --single-branch --branch master git@github.com:maximrdk/terraform-aws-enConnect.git
+git clone --single-branch --branch master git@github.com:maxrodkin/k8-samples.git
 
 
 
