150b64a98bf8a395b3fc27375142d88df7f23461
maxrodkin <maxrodkin@gmail.com>
2021-06-09 14:51:32 +0300

    switch to my personal gcp acc

--- tf-gcp-sshd-tunnels/k8s.tf
+++ tf-gcp-sshd-tunnels/k8s.tf
@@ -1,10 +1,12 @@
 ################## PROVIDERS ############################3
 
 provider "kubernetes" {
-  host  = "https://${data.google_container_cluster.my_cluster.endpoint}"
+  #host  = "https://${data.google_container_cluster.mycluster.endpoint}"
+  host  = "https://${google_container_cluster.mycluster.endpoint}"
   token = data.google_client_config.provider.access_token
   cluster_ca_certificate = base64decode(
-    data.google_container_cluster.my_cluster.master_auth[0].cluster_ca_certificate,
+    #data.google_container_cluster.mycluster.master_auth[0].cluster_ca_certificate,
+    google_container_cluster.mycluster.master_auth[0].cluster_ca_certificate,
   )
 }
 
--- tf-gcp-sshd-tunnels/main.tf
+++ tf-gcp-sshd-tunnels/main.tf
@@ -1,18 +1,20 @@
 ################## PROVIDERS ############################3
 provider "google" {
-  project     = "gcp-ushi-search-platform-npe"
+#  project     = "gcp-ushi-search-platform-npe"
+  project     = "formal-cabinet-316308"
   region      = "us-east4"
-  credentials = "/Users/mrodkin//Documents/gcp/sa-search-datauser-npe.json"
+  credentials = "/Users/mrodkin/Documents/gcp/sa-search-datauser-npe.json"
+#  credentials = "/Users/mrodkin/Downloads/formal-cabinet-316308-85ef2ad7a746.json"
 }
 
 ################## DATA ############################3
-data "google_container_cluster" "my_cluster" {
+data "google_container_cluster" "mycluster" {
   name     = "gke-apollo-us-east4-dev"
   location = "us-east4"
 }
 data "google_client_config" "provider" {}
 
-data "google_compute_network" "default" {
+data "google_compute_network" "vpc_network" {
   name = "gcp-ushi-east4-dgtl-npe-vpc"
 }
 
@@ -22,11 +24,20 @@ variable "project_name" {
 }
 
 ################## RESOURCE ############################
+resource "google_container_cluster" "mycluster" {
+  name = "mycluster"
+  #location = "us-central1"
+  remove_default_node_pool = true
+  initial_node_count       = 1
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "vpc-network"
+}
+
 resource "google_compute_firewall" "default" {
-  name       = var.project_name
-#  network    = data.google_compute_network.default.name
-  network    = "gcp-ushi-east4-dgtl-npe-vpc"
-  depends_on = [data.google_compute_network.default]
+  name    = var.project_name
+  network = google_compute_network.vpc_network.name
   allow {
     protocol = "icmp"
   }
@@ -43,6 +54,6 @@ resource "google_compute_firewall" "default" {
 
 ################## OUTPUT ############################3
 output "google_compute_network-default-name" {
-  value = data.google_compute_network.default.name
+  value = google_compute_network.vpc_network.name
 }
 
